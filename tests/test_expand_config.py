054e14ecfd0cc3a5a289b4a0d0ddfcbfb9c594fe
David Voswinkel <david@optixx.org>
2024-10-16 16:39:21 +0000

    Add test for expand config

---
+++ tests/test_expand_config.py
@@ -0,0 +1,24 @@
+import pytest
+from pathlib import Path
+from retrosync import expand_config
+
+
+def test_expand_config():
+    default = {
+        "src_playlists": "~/playlists",
+        "src_bios": "~/bios",
+        "src_config": "~/config",
+        "src_roms": "~/roms",
+        "src_cores": "~/cores",
+        "src_thumbnails": "~/thumbnails",
+        "dest_playlists": "~/dest_playlists",
+        "dest_bios": "~/dest_bios",
+        "dest_config": "~/dest_config",
+        "dest_roms": "~/dest_roms",
+        "dest_thumbnails": "~/dest_thumbnails",
+    }
+
+    expanded = expand_config(default)
+
+    for key, value in default.items():
+        assert expanded[key] == str(Path(value).expanduser())
